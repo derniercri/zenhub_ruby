4189e0be36b16f089155b0a6daed7499f1132151
Michael Yagudaev <michael@yagudaev.com>
2017-04-25 22:57:36 -0700

    add estimate handling api

--- lib/zenhub_ruby/client/api.rb
+++ lib/zenhub_ruby/client/api.rb
@@ -12,6 +12,10 @@ module ZenhubRuby
       def board_data(repo_name)
         get "/p1/repositories/#{github.repo_id(repo_name)}/board"
       end
+
+      def update_estimate(repo_name, issue_number, estimate)
+        put "/p1/repositories/#{github.repo_id(repo_name)}/issues/#{issue_number}/estimate"
+      end
     end
   end
 end
